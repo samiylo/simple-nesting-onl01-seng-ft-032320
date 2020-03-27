d4730cb129d6ec520894afb2ce418f3b42a0772a
Samiylo Kryshu <skryshu@gmail.com>
2020-03-27 17:33:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
         }
      }
      
-     return programmer_hash[:dennis_ritchie][:languages]
+     return 
+      puts programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
